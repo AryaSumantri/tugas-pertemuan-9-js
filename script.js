d05daf0595aa6595ad1a38d26720ee563e7f1f56
Aryadi Sumantri <147815087+AryaSumantri@users.noreply.github.com>
2023-10-22 23:03:54 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,140 @@
+class Ulul_azmi {
+  constructor() {
+    this.nama = "";
+    this.tugas = "";
+    this.sifat = "";
+    this.ujian = "";
+    this.umur = "";
+    this.mukjizat = "";
+  }
+
+  mengajak(nama) {
+    console.log(nama + " mengajak orang2 untuk");
+  }
+
+  menyampaikan(nama) {
+    console.log(nama + "tidak pernah berhenti menyampaikan");
+  }
+
+  berkontrak(nama) {
+    console.log(nama + "Menerima perjanjian dan juga wasiat dari ALLAH");
+  }
+
+  mengajarkan(nama) {
+    console.log(nama + "Mengajarkan ajaran islam yang sudah ALLAH wahyukan");
+  }
+
+  memperoleh(nama) {
+    console.log(nama + "Memperoleh");
+  }
+
+  berdoa(nama) {
+    console.log(
+      nama +
+        "Selalu berdoa kepada ALLAH agar diberikan hidayah dan kekuatan untuk menghadapi kaum"
+    );
+  }
+
+  berbicara(nama) {
+    console.log(nama + "selalu berbicara jujur");
+  }
+
+  melakukan(nama) {
+    console.log(nama + "Melakukan kebaikan");
+  }
+}
+
+NabiNuh = new Ulul_azmi();
+let nama = (NabiNuh.nama = "Nabi Nuh Alaihissalam");
+let tugas = (NabiNuh.tugas =
+  "Nabi Nuh diutus oleh Allah untuk menyerukan ajaran Allah pada umat Bani Rasib yang menyembah patung-patung berhala");
+let sifat = (NabiNuh.sifat = "Satu di antara banyak sifat terpuji yang dimiliki Nabi Nuh yang paling dikenal adalah ia merupakan pribadi yang sangat pandai bersyukur.");
+let ujian = (NabiNuh.ujian = "Anak dan istrinya menentang ajaran islam");
+let umur = (NabiNuh.umur = "Menurut mufassir Ibnu Abbas RA, Nabi Nuh AS berumur sangat panjang yaitu 1.050 tahun dan berdakwah selama 950 tahun.");
+let keistimewaan = (NabiNuh.mukjizat = "Membuat kapal besar di atas gunung");
+let ajak = (NabiNuh.mengajak =
+  "Sedang mengajak orang2 untuk kembali kepada ajaran islam");
+let wahyu = (NabiNuh.menyampaikan =
+  "Sedang menyampaikan bahwa akan datang banjir bandang");
+let wasiat = (NabiNuh.berkontrak = "Sedang menerima wasiat dari ALLAH");
+let melakukan = (NabiNuh.melakukan =
+  "Sedang membuat kapal yang sangat besar di atas gunung");
+let berdoa = (NabiNuh.berdoa =
+  "Sedang berdoa kepada ALLAH agar kaumnya diberikan petunjuk dan hidayah");
+let berbicara = (NabiNuh.berbicara =
+  "Sedang berbicara kepada kaumnya yang sholeh bahwa akan ada banjir besar");
+
+NabiIbrahim = new Ulul_azmi();
+nama = NabiIbrahim.nama = "Nabi Ibrahim Alaihissalam";
+tugas = NabiIbrahim.tugas =
+  "Nabi Ibrahim memiliki tugas yang berat, yaitu mengajarkan ketauhidan kepada masyarakat jahiliyah yang telah memiliki keyakinan kuat terhadap penyembahan berhala.";
+sifat = NabiIbrahim.sifat = "Nabi Ibrahim adalah sosok yang rasional dalam mencari kebenaran.";
+ujian = NabiIbrahim.ujian = "Salah satu cobaan Nabi Ibrahim AS adalah ketika harus mengorbankan putra kesayangannya untuk disembelih";
+umur = NabiIbrahim.umur = "175 tahun";
+keistimewaan = NabiIbrahim.mukjizat =
+  "Jari Nabi Ibrahim dapat mengeluarkan susu dan madu"
+ajak = NabiIbrahim.mengajak =
+  "Sedang mengajak orang2 untuk beriman kepada ALLAH";
+wahyu = NabiIbrahim.menyampaikan = "Sedang menyampaikan wahyu ALLAH";
+wasiat = NabiIbrahim.berkontrak = "Sedang menerima wasiat dari ALLAH";
+berdoa = NabiIbrahim.berdoa =
+  "Sedang berdoa kepada ALLAH agar kaumnya diberikan petunjuk dan hidayah";
+berbicara = NabiIbrahim.berbicara =
+  "Sedang berbicara kepada berhala raja namrud";
+melakukan = NabiIbrahim.melakukan =
+  "Sedang memenggal kepala berhalanya raja namrud";
+
+NabiIMusa = new Ulul_azmi();
+nama = NabiIMusa.nama = "Nabi Musa Alaihissalam";
+tugas = NabiIMusa.tugas =
+  "Nabi musa as mendapat tugas dari allah swt untuk menyadarkan firaun dari kekeliruan yang menyakan dirinya sebagai tuhan.";
+sifat = NabiIMusa.sifat = "Pemberani dalam menyampaikan kebenaran";
+ujian = NabiIMusa.ujian = "Menghadapi Firaun";
+umur = NabiIMusa.umur = "Nabi Musa AS wafat di usia 120 tahun.";
+keistimewaan = NabiIMusa.mukjizat = "tongkat Nabi Musa as bisa berubah menjadi ular dan membelah lautan.";
+ajak = NabiIMusa.mengajak =
+  "Sedang mengajak orang2 untuk meyakini bahwa ALLAH sesungguhnya tuhan";
+wahyu = NabiIMusa.menyampaikan = "Sedang menyampaikan wahyu ALLAH";
+wasiat = NabiIMusa.berkontrak = "Sedang menerima wasiat dari ALLAH";
+berdoa = NabiIMusa.berdoa =
+  "Sedang berdoa kepada ALLAH agar kaumnya diberikan petunjuk dan hidayah";
+berbicara = NabiIMusa.berbicara = "Sedang berbicara kepada raja firaun";
+melakukan = NabiIMusa.melakukan = "Sedang membelah lautan";
+
+NabiIsa = new Ulul_azmi();
+nama = NabiIsa.nama = "Nabi Isa Alaihissalam";
+tugas = NabiIsa.tugas = "Membunuh Dajjal dan Ya'juj Ma'juj. ";
+sifat = NabiIsa.sifat = "Selain itu Nabi Isa adalah nabi yang memiliki iman yang kuat dan yang paling penting adalah tekun beribadah dan selalu malaksanakan perintah Allah SWT agar selamat dunia akhirat.";
+ujian = NabiIsa.ujian = "Memiliki murid dan umat yang berkhianat";
+umur = NabiIsa.umur = " masa 40 tahun adalah umur keseluruhan Nabi Isa AS di bumi, yakni masa hidupnya di bumi sebelum diangkat ke langit";
+keistimewaan = NabiIsa.mukjizat = "Bisa bicara saat masih bayi";
+ajak = NabiIsa.mengajak = "Sedang mengajak orang2 untuk beriman kepada ALLAH";
+wahyu = NabiIsa.menyampaikan = "Sedang menyampaikan wahyu ALLAH";
+wasiat = NabiIsa.berkontrak = "Sedang menerima wasiat dari ALLAH";
+berdoa = NabiIsa.berdoa =
+  "Sedang berdoa kepada ALLAH agar kaumnya diberikan petunjuk dan hidayah";
+berbicara = NabiIsa.berbicara =
+  "Sedang berbicara kepada kaum yahudi ketika masih bayi";
+melakukan = NabiIsa.melakukan = "Sedang memnghidupkan orang mati";
+
+NabiMuhammad = new Ulul_azmi();
+nama = NabiMuhammad.nama = "Nabi Muhammad Shalallahu Alaihi Wasallam";
+tugas = NabiMuhammad.tugas = "Nabi dan rasul merupakan sosok suci dalam penyebaran agama Islam. Peran nabi dan rasul dalam menyebarkan ajaran agama Islam inilah yang bisa menjadi perbedaan utama, yang perlu kamu pahami.";
+sifat = NabiMuhammad.sifat = "Sabar";
+ujian = NabiMuhammad.ujian = "sidik, amanah, fathonah, dan tablig.";
+umur = NabiMuhammad.umur = "Nabi Muhammad SAW wafat pada usia 63 tahun, ";
+keistimewaan = NabiMuhammad.mukjizat = "Kitab Al-Quran";
+ajak = NabiMuhammad.mengajak =
+  "Sedang mengajak orang2 untuk menunaikan sholat,zakat,puasa,dan haji";
+wahyu = NabiMuhammad.menyampaikan = "Sedang menyampaikan wahyu ALLAH";
+wasiat = NabiMuhammad.berkontrak = "Sedang menerima wasiat dari ALLAH";
+berdoa = NabiMuhammad.berdoa =
+  "Sedang berdoa kepada ALLAH agar kaumnya diberikan petunjuk dan hidayah";
+berbicara = NabiMuhammad.berbicara = "Sedang berbicara kepada kaumnya";
+melakukan = NabiMuhammad.melakukan = "Sedang melakukan isra miraj";
+
+const para_nabi = [NabiNuh, NabiIbrahim, NabiIMusa, NabiIsa, NabiMuhammad];
+
+for (nabi of para_nabi) {
+  console.log(nabi);
+}
